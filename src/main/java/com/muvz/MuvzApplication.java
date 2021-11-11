ab13e290ccb331462461138827a09d9dd9d96e0a
fabriciio95 <fabriciusiqueira@gmail.com>
2021-11-11 10:01:10 -0300

    Projeto criado

---
+++ src/main/java/com/muvz/MuvzApplication.java
@@ -0,0 +1,13 @@
+package com.muvz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuvzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuvzApplication.class, args);
+	}
+
+}
